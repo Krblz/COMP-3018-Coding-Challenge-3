1f30cadda44c5ccf841c6990fc3e3e9e3a6132b8
Keith <krobles3@rrc.ca>
2026-02-24 17:49:45 -0600

    Added Router

---
+++ src/api/v1/routes/productRoutes.ts
@@ -0,0 +1,20 @@
+import express, { Router } from "express";
+import {
+    getHealthCheck,
+    getProduct,
+    getSelectedProduct,
+    createProduct, 
+    updateProduct,
+    deleteProduct
+} from "../controllers/productController";
+
+const router: Router = express.Router();
+
+router.get("/api/v1/health", getHealthCheck);
+router.get("/api/v1/products", getProducts);
+router.get("/api/v1/products/:id", getSelectedProduct);
+router.post("/api/v1/products", createProduct);
+router.put("/api/v1/products/:id", updateProduct);
+router.delete("/api/v1/products/:id", deleteProduct);
+
+export default router;
